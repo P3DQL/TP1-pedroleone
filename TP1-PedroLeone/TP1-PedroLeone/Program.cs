ef91329e5e47985f4d4089eb2df5d7a729485662
P3DQL <pedroleone.hall@gmail.com>
2026-07-15 19:41:40 -0300

    Add files via upload

---
+++ TP1-PedroLeone/TP1-PedroLeone/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Configuracion
+{
+    public int Filas;
+    public int Columnas;
+    public int Velocidad;
+
+    public Configuracion()
+    {
+        Filas = 20;
+        Columnas = 10;
+        Velocidad = 100;
+    }
+}
+
+class Copo
+{
+    public int X;
+    public int Y;
+
+    public Copo(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void Mostrar()
+    {
+        Console.SetCursorPosition(X, Y);
+        Console.Write("*");
+    }
+
+    public void Bajar()
+    {
+        Y++;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.CursorVisible = false;
+
+        Configuracion config = new Configuracion();
+        List<Copo> copos = new List<Copo>();
+
+        Random r = new Random();
+
+        while (true)
+        {
+            // Crear un nuevo copo
+            int columna = r.Next(config.Columnas);
+
+            bool ocupado = false;
+
+            foreach (Copo c in copos)
+            {
+                if (c.X == columna && c.Y == 0)
+                {
+                    ocupado = true;
+                }
+            }
+
+            if (!ocupado)
+            {
+                copos.Add(new Copo(columna, 0));
+            }
+
+            // Mover copos
+            for (int i = 0; i < copos.Count; i++)
+            {
+                if (copos[i].Y < config.Filas - 1)
+                {
+                    bool libre = true;
+
+                    foreach (Copo otro in copos)
+                    {
+                        if (otro != copos[i] &&
+                            otro.X == copos[i].X &&
+                            otro.Y == copos[i].Y + 1)
+                        {
+                            libre = false;
+                        }
+                    }
+
+                    if (libre)
+                    {
+                        copos[i].Bajar();
+                    }
+                }
+            }
+
+            // Verificar filas completas
+            for (int fila = config.Filas - 1; fila >= 0; fila--)
+            {
+                int cantidad = 0;
+
+                for (int col = 0; col < config.Columnas; col++)
+                {
+                    foreach (Copo c in copos)
+                    {
+                        if (c.X == col && c.Y == fila)
+                        {
+                            cantidad++;
+                            break;
+                        }
+                    }
+                }
+
+                if (cantidad == config.Columnas)
+                {
+                    // Eliminar fila
+                    copos.RemoveAll(c => c.Y == fila);
+
+                    // Bajar las filas superiores
+                    foreach (Copo c in copos)
+                    {
+                        if (c.Y < fila)
+                        {
+                            c.Y++;
+                        }
+                    }
+                }
+            }
+
+            // Dibujar
+            Console.Clear();
+
+            foreach (Copo c in copos)
+            {
+                c.Mostrar();
+            }
+
+            Thread.Sleep(config.Velocidad);
+        }
+    }
+}
---
+++ TP1-PedroLeone/TP1-PedroLeone/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TP1-PedroLeone")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TP1-PedroLeone")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("01268236-f188-4d98-a362-02116a2d45ee")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
